862796948cf0873dccbfcd051dd04018660d0a65
Roberto Tonino <58312518+BobToninho@users.noreply.github.com>
2024-09-01 13:23:32 +0200

    find

--- find.sh
+++ find.sh
@@ -1,13 +1,18 @@
-data_file=$1
+trap 'echo "Script interrupted"; exit 1' SIGINT SIGTERM
 
-for i in {1..30}
+data_file=${1:-aliens0.lp}
+tries=${2:-20}
+
+echo -e "Running $data_file for $tries times"
+
+for i in $(seq 1 $tries);
 do
-  echo -e "\nt=$i\n"
-  clingo -c t=$i invaders.lp $data_file 1
+  clingo -c t=$i invaders.lp $data_file 1 > /dev/null
   exit_code=$?
 
   case $exit_code in
     10 | 30)
+      echo $i
       exit 0
   esac
 done
