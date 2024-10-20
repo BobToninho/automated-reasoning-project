a4f46cb85b774845017b348326f93358bd5d5e22
Roberto Tonino <58312518+BobToninho@users.noreply.github.com>
2024-10-20 13:17:50 +0000

    update
    Signed-off-by: Roberto Tonino <58312518+BobToninho@users.noreply.github.com>

--- clingo/find.sh
+++ clingo/find.sh
@@ -1,11 +1,12 @@
 trap 'echo "Script interrupted"; exit 1' SIGINT SIGTERM
 
 data_file=${1:-aliens0.lp}
-tries=${2:-20}
+min_tries=${2:-1}
+max_tries=${3:-20}
 
-echo -e "Running $data_file for $tries times"
+echo -e "Running $data_file with t from $min_tries to $max_tries"
 
-for i in $(seq 1 $tries);
+for i in $(seq $min_tries $max_tries);
 do
   clingo -c t=$i invaders.lp $data_file 1 > /dev/null
   exit_code=$?
